e113cb34d28904fa427f1c2150daa22e0c2d4833
unknown <burakavar@gmail.com>
2021-12-17 21:02:21 +0300

    Created big number finding algorithm

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+
+namespace cSharp_Console_Assignments
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rRandom = new Random();
+            int[] numbers = new int[5];
+
+            for(int i=0; i<5; i++){
+                numbers[i] = rRandom.Next(1,20);
+                Console.WriteLine($"{i}st number: {numbers[i]}");
+            }
+
+            Array.Sort(numbers);
+
+            Console.WriteLine($"Big Number : {numbers[4]}");
+            Console.ReadKey();
+            
+        }
+    }
+}
