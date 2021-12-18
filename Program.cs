ca322520716bfdf59c280650244e0d097972cd3d
unknown <burakavar@gmail.com>
2021-12-18 13:26:45 +0300

    method change

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections;
+using System.Linq;
 
 namespace cSharp_Console_Assignments
 {
@@ -7,41 +8,41 @@ namespace cSharp_Console_Assignments
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(SalaryAfterFiveYear(10000,0.15));
+            
+            /*BigNumber(): Kullanıcının klavyeden girdiği dört sayıdan büyük olanının hangisi 
+                           olduğunu ekrana yazan programı yazın. */
+            //Console.WriteLine(BigNumber(new int[4]));
+
+            /*SalaryAfterFiveYear(): Bir çalışanın maaşı 1000 TL’dir ve çalıştığı şirketle 
+                                    yıllık %15 artış ile anlaşmıştır. Beş yıl sonra maaşını 
+                                    hesaplayan programı yazın. */
+            Console.WriteLine(SalaryAfterFiveYear(1000,0.15));
+
+
+
             Console.ReadKey();
         }
 
+        static int BigNumber(int[] numbers){
+
+            for(int i=0; i<numbers.Length; i++){
+
+                Console.WriteLine($"{i}st number :");
+                numbers[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            return numbers.Max();
+        }
         static double SalaryAfterFiveYear(double salary, double percent){
             for(int year=1; year<=5; year++){
                 
-                double increased;
-                increased = salary * percent;
-                salary = salary + increased;
+                double increase;
+                increase = salary * percent;
+                salary = salary + increase;
             }
 
             return salary;
         }
-        static void BigNumber(){
-                        Random rRandom = new Random();
-            int[] numbers = new int[5];
 
-            for(int i=0; i<5; i++){
-                numbers[i] = rRandom.Next(1,20);
-                Console.WriteLine($"{i}st number: {numbers[i]}");
-            }
-
-            Array.Sort(numbers);
-
-            Console.WriteLine($"Big Number : {numbers[4]}");
-            Console.ReadKey();
-        }
-        static void Comments(){
-            // BigNumber(): Kullanıcının klavyeden girdiği dört sayıdan büyük olanının hangisi 
-            //              olduğunu ekrana yazan programı yazın.
-
-            // SalaryAfterFiveYear(): Bir çalışanın maaşı 1000 TL’dir ve çalıştığı şirketle 
-            //                        yıllık %15 artış ile anlaşmıştır. Beş yıl sonra maaşını 
-            //                        hesaplayan programı yazın.
-        }
     } 
 }
