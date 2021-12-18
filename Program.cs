622714009e9bc27f0c9c08dce031803273e5db27
unknown <burakavar@gmail.com>
2021-12-18 13:30:42 +0300

    method change2

--- Program.cs
+++ Program.cs
@@ -11,12 +11,12 @@ namespace cSharp_Console_Assignments
             
             /*BigNumber(): Kullanıcının klavyeden girdiği dört sayıdan büyük olanının hangisi 
                            olduğunu ekrana yazan programı yazın. */
-            //Console.WriteLine(BigNumber(new int[4]));
+            Console.WriteLine(BigNumber(new int[4]));
 
             /*SalaryAfterFiveYear(): Bir çalışanın maaşı 1000 TL’dir ve çalıştığı şirketle 
                                     yıllık %15 artış ile anlaşmıştır. Beş yıl sonra maaşını 
                                     hesaplayan programı yazın. */
-            Console.WriteLine(SalaryAfterFiveYear(1000,0.15));
+            //Console.WriteLine(SalaryAfterFiveYear(1000,0.15));
 
 
 
@@ -24,19 +24,15 @@ namespace cSharp_Console_Assignments
         }
 
         static int BigNumber(int[] numbers){
-
             for(int i=0; i<numbers.Length; i++){
-
                 Console.WriteLine($"{i}st number :");
                 numbers[i] = Convert.ToInt32(Console.ReadLine());
             }
-
             return numbers.Max();
         }
         static double SalaryAfterFiveYear(double salary, double percent){
-            for(int year=1; year<=5; year++){
-                
-                double increase;
+            double increase;
+            for(int year=1; year<=5; year++){  
                 increase = salary * percent;
                 salary = salary + increase;
             }
