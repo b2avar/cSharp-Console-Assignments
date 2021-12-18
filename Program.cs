855f6d20bde9a6426991d65c2e331a21ee124b2f
unknown <burakavar@gmail.com>
2021-12-18 12:24:04 +0300

    methods that return values

--- Program.cs
+++ Program.cs
@@ -7,10 +7,21 @@ namespace cSharp_Console_Assignments
     {
         static void Main(string[] args)
         {
-            BigNumber();
+            Console.WriteLine(SalaryAfterFiveYear(10000,0.15));
+            Console.ReadKey();
         }
 
-        private static void BigNumber(){
+        static double SalaryAfterFiveYear(double salary, double percent){
+            for(int year=1; year<=5; year++){
+                
+                double increased;
+                increased = salary * percent;
+                salary = salary + increased;
+            }
+
+            return salary;
+        }
+        static void BigNumber(){
                         Random rRandom = new Random();
             int[] numbers = new int[5];
 
@@ -24,11 +35,13 @@ namespace cSharp_Console_Assignments
             Console.WriteLine($"Big Number : {numbers[4]}");
             Console.ReadKey();
         }
-        private static void Comment(){
+        static void Comments(){
             // BigNumber(): Kullanıcının klavyeden girdiği dört sayıdan büyük olanının hangisi 
             //              olduğunu ekrana yazan programı yazın.
 
-            
+            // SalaryAfterFiveYear(): Bir çalışanın maaşı 1000 TL’dir ve çalıştığı şirketle 
+            //                        yıllık %15 artış ile anlaşmıştır. Beş yıl sonra maaşını 
+            //                        hesaplayan programı yazın.
         }
     } 
 }
